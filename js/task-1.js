031416326259d035b9b561ed6cc631b0fdad8602
Emin Acar <mimareminacar@gmail.com>
2025-04-15 02:21:16 +0300

    first

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+let quantity = 5;
+let pricePerDroid = 250;
+
+function makeTransaction(quantity, pricePerDroid) {
+    let totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
